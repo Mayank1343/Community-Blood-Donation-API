48bb2ff17d7c7015863f12504fa243dfb11e089f
Mayank1343 <mayanksharma1343@gmail.com>
2026-06-14 15:45:45 +0530

    feat(auth): implement JWT login and token generation

---
+++ blood-donation-api/src/main/java/com/example/blood_donation_api/service/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.example.blood_donation_api.service;
+
+import com.example.blood_donation_api.model.User;
+import com.example.blood_donation_api.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+private final UserRepository userRepository;
+
+public CustomUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+}
+
+@Override
+public UserDetails loadUserByUsername(String username)
+        throws UsernameNotFoundException {
+
+    User user = userRepository.findByUsername(username)
+            .orElseThrow(() ->
+                    new UsernameNotFoundException(
+                            "User not found"));
+
+    return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            List.of(
+                    new SimpleGrantedAuthority(
+                            "ROLE_" + user.getRole().name()
+                    )
+            )
+    );
+}
+
+}
