ada6907238f898ed4046a6867ef81d2f320db1b3
Mayank1343 <mayanksharma1343@gmail.com>
2026-06-14 16:28:27 +0530

    feat(auth): integrate JWT authentication filter

--- blood-donation-api/src/main/java/com/example/blood_donation_api/config/SecurityConfig.java
+++ blood-donation-api/src/main/java/com/example/blood_donation_api/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.blood_donation_api.security.JwtAuthenticationFilter;
 import com.example.blood_donation_api.service.CustomUserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
@@ -20,25 +24,34 @@ public PasswordEncoder passwordEncoder() {
 
 private final CustomUserDetailsService userDetailsService;
 
-public SecurityConfig(CustomUserDetailsService userDetailsService) {
+public SecurityConfig(
+        CustomUserDetailsService userDetailsService,
+        JwtAuthenticationFilter jwtAuthenticationFilter) {
+
     this.userDetailsService = userDetailsService;
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
 }
+
+private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http)
         throws Exception {
 
     http
-            .csrf(csrf -> csrf.disable())
+        .csrf(csrf -> csrf.disable())
 
-            .userDetailsService(userDetailsService)
+        .userDetailsService(userDetailsService)
 
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth/**").permitAll()
-                    .anyRequest().authenticated()
-            )
+        .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+        )
 
-            .httpBasic(Customizer.withDefaults());
+        .addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
 
     return http.build();
 }
